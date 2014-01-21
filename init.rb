d7598cd9c990bd6d36d03b80f18ca5c77c676b05
Massimo Rossello <massimo.rossello@artistii.com>
2014-01-21 10:47:31 +0100

    Added Application admin menu. Bump to version 1.0.0

--- init.rb
+++ init.rb
@@ -6,12 +6,13 @@ Redmine::Plugin.register :redmine_app__space do
   name 'Redmine Application Space plugin'
   author 'Massimo Rossello'
   description 'Creates a global application space with configurable application menu entries'
-  version '0.0.4'
+  version '1.0.0'
   url 'https://github.com/maxrossello/redmine_app__space.git'
   author_url 'https://github.com/maxrossello'
   requires_redmine :version_or_higher => '2.0.0'
 
   menu :top_menu, :appSpace, { :controller => 'appspace', :action => 'index', :tab => nil }, :caption => :label_applications, :if => Proc.new { User.current.logged? }
+  menu :admin_menu, :appSpace, {:controller => 'settings', :action => 'plugin', :id => "redmine_app__space"}, :caption => :label_applications
 
   unless User.included_modules.include?(AppspaceUsersPatch)
     User.send(:include, AppspaceUsersPatch)
